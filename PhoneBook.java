f449b90a93f763c11afa9cfbe7466b1922ba5fcb
ilya_k <ivk68@list.ru>
2023-06-03 13:11:37 +0300

    PhoneBook.java

---
+++ PhoneBook.java
@@ -0,0 +1,35 @@
+package HW_5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PhoneBook {
+    private Map<String, String> book;
+
+    public PhoneBook() {
+        book = new HashMap<String, String>();
+    }
+
+    public void add(String name, String phoneNumber) {
+        if (book.containsKey(name)) {
+            String existingNumber = book.get(name);
+            book.put(name, existingNumber + ", " + phoneNumber);
+        } else {
+            book.put(name, phoneNumber);
+        }
+    }
+
+    public String get(String name) {
+        return book.get(name);
+    }
+
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("John Smith", "123-456-7890");
+        phoneBook.add("Jane Smith", "111-222-3333");
+        phoneBook.add("John Smith", "555-666-7777");
+
+        System.out.println(phoneBook.get("John Smith"));
+        System.out.println(phoneBook.get("Jane Smith"));
+    }
+}
